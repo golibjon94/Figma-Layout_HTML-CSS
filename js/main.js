e7fb7193b7da23328308c25d300c9af66ca2d1d2
golibjon94 <golib94jon@gmail.com>
2021-02-04 22:26:49 +0500

    I worked on adapting the project to different devices

--- js/main.js
+++ js/main.js
@@ -2,9 +2,23 @@
         const header = document.querySelector('.header');
         window.onscroll = () => {
                 if(window.pageYOffset > 50) {
-                        header.classList.add('header__avtive');
+                        header.classList.add('header__active');
                 } else {
-                        header.classList.remove('header__avtive');
+                        header.classList.remove('header__active');
                 }
         };
+}());
+
+// Burger handler
+
+(function () {
+        const burgerItem = document.querySelector('.burger');
+        const menu = document.querySelector('.header__nav');
+        const menuCloseItem = document.querySelector('.header__nav-close');
+        burgerItem.addEventListener('click', () => {
+                menu.classList.add('.header__nav-active')
+        });
+        menuCloseItem.addEventListener('click', () => {
+                menu.classList.remove('.header__nav-active')
+        })
 }());
